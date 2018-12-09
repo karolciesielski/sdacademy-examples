fabaad685cb88e28f72f5442bab1f363e29019c9
Karol Ciesielski <karol.ciesielski87@gmail.com>
2018-12-09 14:17:20 +0100

    "9.12.2018 Annotation training"

---
+++ 15_adnotacje/src/main/java/pl/sdacademy/ownannotation/BenchmarkProcessor.java
@@ -0,0 +1,20 @@
+package pl.sdacademy.ownannotation;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class BenchmarkProcessor {
+
+    void benchmark(Object obj) throws InvocationTargetException, IllegalAccessException {
+        long time;
+        Method[] methods = obj.getClass().getDeclaredMethods();
+
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(BenchmarkTime.class)) {
+                time = System.currentTimeMillis();
+                method.invoke(obj);
+                System.out.println(String.format("Method %s takes %d ms to execute", method.getName(), System.currentTimeMillis() - time));
+            }
+        }
+    }
+}
---
+++ 15_adnotacje/src/main/java/pl/sdacademy/ownannotation/TestClass.java
@@ -0,0 +1,9 @@
+package pl.sdacademy.ownannotation;
+
+@FirstAnnotation (lenght = 5)
+public class TestClass {
+
+    @FirstAnnotation (value = "Anything", lenght = 8)
+    int test;
+
+}
